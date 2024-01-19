fc18583d918aee900bddf65f68d5b82d26430c4c
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2024-01-19 11:43:26 +0000

    Deploying to gh-pages from @ ValeriaSihr/project_food@a0ff4180db6f7588a56babd3096ec59241613afd 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,20 @@
-/* empty css                      */import{a as c}from"./assets/vendor-0e3e9dc0.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const l=async()=>{const{data:i}=await c.get("https://food-boutique.b.goit.study/api/products");console.log(i)};l();
+/* empty css                      */import{a as p}from"./assets/vendor-a2e8d7fa.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const n of t)if(n.type==="childList")for(const s of n.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function r(t){const n={};return t.integrity&&(n.integrity=t.integrity),t.referrerpolicy&&(n.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?n.credentials="include":t.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function i(t){if(t.ep)return;t.ep=!0;const n=r(t);fetch(t.href,n)}})();const m=p.create({baseURL:"https://food-boutique.b.goit.study/api"}),a=async o=>{try{const{data:e}=await m(o);return e}catch(e){console.log(e.message)}},f=async()=>await a("/products"),g=async()=>(await a("/products/discount")).sort(()=>Math.random()-.5).slice(0,2);g().then(console.log);const y=async()=>{const{results:o}=await f(),e=o.map(({category:i,img:t,is10PercentOff:n,name:s,popularity:d,price:l,size:u,_id:b})=>`<li>
+  <div class="card-img"><img src="${t}" alt="${s}" /></div>
+  <div class="description">
+    <h3>${s}</h3>
+    
+      <span>Category: </span><span>${i.split("_").join(" ")}</span>
+      <span>Size: </span><span>${u}</span>
+      <span>Popularity: </span><span>${d}</span>
+    
+  </div>
+  <div class="to-cart">
+    <p>${l}</p>
+    <button class="cart-btn" type="button">
+    <svg class="cart-svg" width="18" height="18">
+    <use href="../img/icons.svg#icon-heroicons-solid_shopping-cart"></use></svg>
+    </button>
+  </div>
+</li>
+  `).join(""),r=document.createElement("ul");return r.innerHTML=e,r},c=document.querySelector(".main-cards");document.querySelector(".popular");document.querySelector(".discount");async function h(){const o=await y();c.insertAdjacentElement("beforeend",o);const e=document.createElement("h2");e.textContent="All Products",e.classList.add("hiden-title"),c.insertAdjacentElement("afterbegin",e)}h();
 //# sourceMappingURL=commonHelpers2.js.map
