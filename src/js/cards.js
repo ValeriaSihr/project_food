46d44a38f6ef368a6ec5d6594e50e2a836fe5b00
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2024-03-05 12:13:40 +0000

    dot

--- src/js/cards.js
+++ src/js/cards.js
@@ -31,7 +31,7 @@ export const mainCardsMarkup = async () => {
     <p class="price"> &dollar;${price}</p>
     <button class="cart-btn" type="button">
       <svg class="cart-svg" width="18" height="18">
-        <use href="../img/icons.svg#icon-heroicons-solid_shopping-cart"></use>
+        <use href="./img/icons.svg#icon-heroicons-solid_shopping-cart"></use>
       </svg>
     </button>
   </div>
