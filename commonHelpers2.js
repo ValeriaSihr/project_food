3d2b543c2f4de00a47e8e0c80e2a5d022b5673b3
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2024-03-05 12:14:01 +0000

    Deploying to gh-pages from @ ValeriaSihr/project_food@46d44a38f6ef368a6ec5d6594e50e2a836fe5b00 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -16,7 +16,7 @@
     <p class="price"> &dollar;${i}</p>
     <button class="cart-btn" type="button">
       <svg class="cart-svg" width="18" height="18">
-        <use href="../img/icons.svg#icon-heroicons-solid_shopping-cart"></use>
+        <use href="./img/icons.svg#icon-heroicons-solid_shopping-cart"></use>
       </svg>
     </button>
   </div>
