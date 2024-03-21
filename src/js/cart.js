4cfb8b26f20f5dc034bdf22cd99af6b66a3d99a7
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2024-03-21 14:21:25 +0000

    cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,26 @@
 const CART = 'cart';
-export const addProduct = product => {
-  const isEmpty = localStorage.getItem(CART);
-  let prodArr;
-  if (!isEmpty) {
-    prodArr = [];
-  } else {
-    prodArr = JSON.parse(isEmpty);
+export const getCart = () => {
+  const shoppingCart = localStorage.getItem(CART);
+
+  if (!shoppingCart) {
+    return [];
   }
+  return JSON.parse(shoppingCart);
+};
+
+export const addProduct = product => {
+  const prodArr = getCart();
   prodArr.push(product);
   localStorage.setItem(CART, JSON.stringify(prodArr));
 };
+
+export const removeProd = id => {
+  const prodArr = getCart();
+  const newProductArr = prodArr.filter(product => id !== product._id);
+  localStorage.setItem(CART, JSON.stringify(newProductArr));
+};
+
+export const removeAllProducts = () => {
+  localStorage.removeItem(CART);
+};
+// підключити до корзини
--- src/js/modalcontent.js
+++ src/js/modalcontent.js
@@ -1,22 +1,37 @@
 export const createContent = product => {
-  const { category, img, is10PercentOff, name, desc, popularity, price, size } =
-    product;
+  const {
+    category,
+    img,
+    is10PercentOff,
+    name,
+    desc,
+    popularity,
+    price,
+    size,
+    _id,
+  } = product;
   const picture = document.createElement('img');
   picture.src = img;
   picture.alt = name;
   picture.classList.add('card-img');
+
   console.log(product);
 
   const cost = document.createElement('p');
   cost.innerHTML = is10PercentOff
-    ? `<span class="old-price">${(price / 0.9).toFixed(2)}</span> ${price}`
-    : price;
+    ? `<span class="old-price">&dollar;${(price / 0.9).toFixed(
+        2
+      )}</span>&dollar;${price}`
+    : `&dollar;${price}`;
+  cost.classList.add('price-modal');
   // to make dicsount visible
 
   const button = document.createElement('button');
   button.setAttribute('type', 'button');
   button.textContent = 'Add to';
+  button.classList.add('cart-add');
   const cartSvg = document.createElement('svg');
+  cartSvg.setAttribute('width', '20px');
   const useSvg = document.createElement('use');
   useSvg.setAttribute(
     'href',
@@ -25,42 +40,48 @@ export const createContent = product => {
   // useSvg.href = '../img/icons.svg#icon-heroicons-solid_shopping-cart'; can be used sometimes
   cartSvg.insertAdjacentElement('beforeend', useSvg);
   button.insertAdjacentElement('beforeend', cartSvg);
+  cartSvg.classList.add('cart-svg');
+  useSvg.classList.add('cart-use');
 
   const pricingWrapper = document.createElement('div');
   pricingWrapper.insertAdjacentElement('beforeend', cost);
   pricingWrapper.insertAdjacentElement('beforeend', button);
+  pricingWrapper.classList.add('div-price');
 
   const title = document.createElement('h3');
   title.textContent = name;
+  title.classList.add('product-name');
 
   const description = document.createElement('p');
   description.textContent = desc;
+  description.classList.add('modal-text');
 
   const categorySpan = document.createElement('span');
   const categoryValueSpan = document.createElement('span');
 
   categorySpan.textContent = 'Category: ';
   categoryValueSpan.textContent = category.split('_').join(' ');
-  categorySpan.classList.add('prod-info');
-  categoryValueSpan.classList.add('prod-info-api');
+  categorySpan.classList.add('prod-info-modal');
+  categoryValueSpan.classList.add('prod-info-api-modal');
 
   const prodSize = document.createElement('span');
   const prodValueSize = document.createElement('span');
 
   prodSize.textContent = 'Size: ';
   prodValueSize.textContent = size;
-  prodSize.classList.add('prod-info');
-  prodValueSize.classList.add('prod-info-api');
+  prodSize.classList.add('prod-info-modal');
+  prodValueSize.classList.add('prod-info-api-modal');
 
   const prodPopularity = document.createElement('span');
   const popularityValue = document.createElement('span');
 
   prodPopularity.textContent = 'Popularity: ';
   popularityValue.textContent = popularity;
-  prodPopularity.classList.add('prod-info');
-  popularityValue.classList.add('prod-info-api');
+  prodPopularity.classList.add('prod-info-modal');
+  popularityValue.classList.add('prod-info-api-modal');
 
   const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
   wrapper.insertAdjacentElement('beforeend', picture);
   wrapper.insertAdjacentElement('beforeend', title);
   wrapper.insertAdjacentElement('beforeend', categorySpan);
@@ -71,6 +92,7 @@ export const createContent = product => {
   wrapper.insertAdjacentElement('beforeend', popularityValue);
   wrapper.insertAdjacentElement('beforeend', description);
   wrapper.insertAdjacentElement('beforeend', pricingWrapper);
+  //wrapper.insertAdjacentElement('beforeend', button);
 
   return wrapper;
 };
