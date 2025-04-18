5e5c3ebf60319e6849dddff85918905484f1aa66
Valeria Sihrianska <132007746+ValeriaSihr@users.noreply.github.com>
2025-04-18 14:03:11 +0100

    creating-cart

--- src/js/cartcontent.js
+++ src/js/cartcontent.js
@@ -1,6 +1,8 @@
 import { getCart } from './cart';
 
 const cartTitle = document.querySelector('.prod-cart-title');
+const cartContent = document.querySelector('.cart-content')
+console.log(cartContent);
 
 function fullCartContent() {
   console.log('full');
@@ -14,4 +16,11 @@ export function renderCart() {
   const cart = getCart();
   cartTitle.textContent = `CART (${cart.length})`;
   cart.length > 0 ? fullCartContent() : emptyCartContent();
+  cartContent.insertAdjacentHTML('beforeend', () => {
+    const cartMarkup = cart.map(product => {
+      return `<li></li>`
+    })
+  })
+
 }
+
